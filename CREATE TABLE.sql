0ef4150ae4ec085d5c02abbaf77cb275f7e5fa8a
Ken Quejadas <118844156+kenquejadas@users.noreply.github.com>
2023-05-04 11:47:15 -0500

    Add files via upload

---
+++ CREATE TABLE.sql
@@ -0,0 +1,62 @@
+/* Short Description: Relational database for gym workout data */
+
+CREATE TABLE Log (
+    LogID INT,
+    LogDateTime DATETIME,
+    MatchID INT,
+    Log_Sets INT,
+    Log_Reps INT,
+    Log_Weight INT,
+    Log_Time VARCHAR(10), /* Format: 12:34:56.78 */
+    Log_Incline INT, /* Treadmill incline */
+    Log_Level INT, /* Level for elliptical and stair master */
+    PRIMARY KEY (LogID),
+    FOREIGN KEY (MatchID) REFERENCES Categorize(MatchID)
+);
+
+/* Workout = Category of either cardio or muscles worked */
+CREATE TABLE Workout (
+    WorkoutID INT,
+    WorkoutName VARCHAR(20) NOT NULL, /* Cardio, Push, Pull, Abs, Leg Day */
+    ExerciseID INT,
+    PRIMARY KEY (WorkoutID),
+    FOREIGN KEY (ExerciseID) REFERENCES Exercise(ExerciseID)
+);
+
+CREATE TABLE Exercise (
+    ExerciseID INT,
+    ExerciseName VARCHAR(20) NOT NULL, /* Weights, Treadmill, Stair Master, Elliptical*/
+    MatchID INT,
+    ExerciseType VARCHAR(10),
+    Equipment VARCHAR(10),
+    PRIMARY KEY (ExerciseID),
+    FOREIGN KEY (MatchID) REFERENCES Categorize(MatchID),
+    FOREIGN KEY (ExerciseType) REFERENCES ExerciseType(ExerciseType),
+    FOREIGN KEY (Equipment) REFERENCES Equipment(Equipment)
+);
+
+/* Separate table for exercise type so that each exercise has their respective type
+	and prevent duplicates */
+CREATE TABLE ExerciseType( 
+	ExerciseType VARCHAR(10) /* Weights, Walk, Run, Climb */
+);
+
+/* Separate table for equipment so that each exercise has their respective equipment
+	and prevent duplicates */
+CREATE TABLE Equipment( 
+    Equipment VARCHAR(10) /* Machine, Dumbbells, Barbells, Band */
+);
+
+CREATE TABLE Categorize ( /* Categorize exercises by matching them with workout type */
+	MatchID INT,
+    WorkoutType VARCHAR(10),
+    ExerciseName VARCHAR(20),
+    PRIMARY KEY (MatchID),
+    FOREIGN KEY (WorkoutType) REFERENCES Workout(WorkoutType),
+    FOREIGN KEY (ExerciseName) REFERENCES Exercise(ExerciseName)
+);
+
+/* Example Query: Find out what exercises were done last Monday
+    
+    SELECT ExerciseName
+ */
