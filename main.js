f6bd707efad4c074bc44161400e5d83b724c53c4
Ken Quejadas <118844156+kenquejadas@users.noreply.github.com>
2023-07-10 13:30:56 -0500

    Search bar function

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
